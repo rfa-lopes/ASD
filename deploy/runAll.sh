e6ff16ffbf095efe166a3631faad5c13c3f7c239
Rodrigo Lopes <rfa.lopes@campus.fct.unl.pt>
2020-11-14 21:44:57 +0000

    update

--- deploy/runAll.sh
+++ deploy/runAll.sh
@@ -9,26 +9,26 @@ mkdir -p Logs-Cluster
 
 #ALL COMBINATIONS
 echo "################## EAGER + HYPARVIEW..."
-#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=3000 
-#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## EAGER + CYCLON..."
-#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=3000 
-#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## PLUMTREE + CYCLON..."
-#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=3000 
-#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## PLUMTREE + HYPARVIEW..."
-#./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=1000 
-#./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=3000 
+./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=1000 
+./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=3000 
 ./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
 ./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
 
