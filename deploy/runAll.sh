413e1cca4b0a66916d1d676b4d1ef35437795c9f
Rodrigo Lopes <rfa.lopes@campus.fct.unl.pt>
2020-11-14 19:34:00 +0000

    Logs

--- deploy/runAll.sh
+++ deploy/runAll.sh
@@ -9,26 +9,26 @@ mkdir -p Logs-Cluster
 
 #ALL COMBINATIONS
 echo "################## EAGER + HYPARVIEW..."
-./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=3000 
-./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=eager --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## EAGER + CYCLON..."
-./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=3000 
-./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=eager --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## PLUMTREE + CYCLON..."
-./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=3000 
-./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
 
 echo "################## PLUMTREE + HYPARVIEW..."
-./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=3000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=1000 
+#./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10 --broadcast_interval=3000 
 ./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10000 --broadcast_interval=1000 
 ./deploy/runTest.sh --broadcast=plumtree --membership=hyparview --payload_size=10000 --broadcast_interval=3000 
 
@@ -44,15 +44,3 @@ echo "################## CYCLON..."
 ./deploy/runTest.sh --broadcast="" --membership=cyclon --payload_size=10 --broadcast_interval=3000 
 ./deploy/runTest.sh --broadcast="" --membership=cyclon --payload_size=10000 --broadcast_interval=1000 
 ./deploy/runTest.sh --broadcast="" --membership=cyclon --payload_size=10000 --broadcast_interval=3000 
-
-echo "################## PLUMTREE..."
-./deploy/runTest.sh --broadcast=plumtree --membership="" --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=plumtree --membership="" --payload_size=10 --broadcast_interval=3000 
-./deploy/runTest.sh --broadcast=plumtree --membership="" --payload_size=10000 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=plumtree --membership="" --payload_size=10000 --broadcast_interval=3000 
-
-echo "################## EAGER..."
-./deploy/runTest.sh --broadcast=eager --membership="" --payload_size=10 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership="" --payload_size=10 --broadcast_interval=3000 
-./deploy/runTest.sh --broadcast=eager --membership="" --payload_size=10000 --broadcast_interval=1000 
-./deploy/runTest.sh --broadcast=eager --membership="" --payload_size=10000 --broadcast_interval=3000 
--- scripts/checkLogs.sh
+++ scripts/checkLogs.sh
@@ -26,3 +26,31 @@ ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10/broadcast_interval-300
 ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10000/broadcast_interval-1000/logs | wc -l
 ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10000/broadcast_interval-3000/logs | wc -l
 echo
+
+echo "---"
+
+echo "hyparview"
+ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10/broadcast_interval-3000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10000/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+hyparview/payload_size-10000/broadcast_interval-3000/logs | wc -l
+
+echo "cyclon"
+ls -la ../Logs-Cluster/+cyclon/payload_size-10/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/+cyclon/payload_size-10/broadcast_interval-3000/logs | wc -l
+ls -la ../Logs-Cluster/+cyclon/payload_size-10000/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/+cyclon/payload_size-10000/broadcast_interval-3000/logs | wc -l
+
+echo "plumtree"
+ls -la ../Logs-Cluster/plumtree+/payload_size-10/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+/payload_size-10/broadcast_interval-3000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+/payload_size-10000/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/plumtree+/payload_size-10000/broadcast_interval-3000/logs | wc -l
+echo
+
+echo "eager"
+ls -la ../Logs-Cluster/eager+/payload_size-10/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/eager+/payload_size-10/broadcast_interval-3000/logs | wc -l
+ls -la ../Logs-Cluster/eager+/payload_size-10000/broadcast_interval-1000/logs | wc -l
+ls -la ../Logs-Cluster/eager+/payload_size-10000/broadcast_interval-3000/logs | wc -l
+echo
