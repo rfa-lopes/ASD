66c86b458f33adc6159d65b1efb3c03b05811b81
Rodrigo Lopes <rfa.lopes@campus.fct.unl.pt>
2020-11-29 12:59:17 +0000

    Multi-Paxos init - Prepare and PrepareOk messages

---
+++ phase2/src/main/java/protocols/agreement/messages/PrepareMessage.java
@@ -0,0 +1,43 @@
+package protocols.agreement.messages;
+
+import io.netty.buffer.ByteBuf;
+import pt.unl.fct.di.novasys.babel.generic.ProtoMessage;
+import pt.unl.fct.di.novasys.network.ISerializer;
+import java.io.IOException;
+
+/*Made by Rodrigo*/
+public class PrepareMessage extends ProtoMessage {
+
+    public final static short MSG_CODE = 9021;
+
+    private final int sequenceNumber;
+
+    public PrepareMessage(int sequenceNumber) {
+        super(MSG_CODE);
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PrepareMessage{" +
+                "sequenceNumber: " + sequenceNumber +
+                "}";
+    }
+
+    public static final ISerializer<PrepareMessage> serializer = new ISerializer<PrepareMessage>()  {
+        @Override
+        public void serialize(PrepareMessage prepareMessage, ByteBuf out) throws IOException {
+            out.writeInt(prepareMessage.getSequenceNumber());
+        }
+
+        @Override
+        public PrepareMessage deserialize(ByteBuf in) throws IOException {
+            int sequenceNumber = in.readInt();
+            return new PrepareMessage(sequenceNumber);
+        }
+    };
+}
---
+++ phase2/src/main/java/protocols/agreement/messages/PrepareOkMessage.java
@@ -0,0 +1,32 @@
+package protocols.agreement.messages;
+
+import io.netty.buffer.ByteBuf;
+import pt.unl.fct.di.novasys.babel.generic.ProtoMessage;
+import pt.unl.fct.di.novasys.network.ISerializer;
+import java.io.IOException;
+
+/*Made by Rodrigo*/
+public class PrepareOkMessage extends ProtoMessage {
+
+    public final static short MSG_CODE = 9022;
+
+    public PrepareOkMessage() {
+        super(MSG_CODE);
+    }
+
+    @Override
+    public String toString() {
+        return "PrepareOkMessage{}";
+    }
+
+    public static final ISerializer<PrepareOkMessage> serializer = new ISerializer<PrepareOkMessage>()  {
+        @Override
+        public void serialize(PrepareOkMessage prepareOkMessage, ByteBuf out) throws IOException {
+        }
+
+        @Override
+        public PrepareOkMessage deserialize(ByteBuf in) throws IOException {
+            return new PrepareOkMessage();
+        }
+    };
+}
